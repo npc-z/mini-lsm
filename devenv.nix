8ae2a591db1a368d1961d39d2a8a0a87aa903c0a
npc-z <1763998996@qq.com>
2026-02-20 18:41:08 +0800

    feat: add devenv configuration

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  # packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+}
